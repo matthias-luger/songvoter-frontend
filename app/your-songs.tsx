c2f92796ebe64593d1c16e98d57d630e1f5bd48f
Matthias Luger <malua007@live.de>
2023-07-27 02:27:27 +0200

    add loading animation reload songs after add

--- app/your-songs.tsx
+++ app/your-songs.tsx
@@ -1,7 +1,7 @@
 import MainLayout from '../layouts/MainLayout'
 import { useEffect, useState } from 'react'
 import { ScrollView, StyleSheet, View, Image } from 'react-native'
-import { FAB, IconButton, List, Modal, Portal, Text } from 'react-native-paper'
+import { ActivityIndicator, FAB, IconButton, List, Modal, Portal, Text } from 'react-native-paper'
 import { globalStyles } from '../styles/globalStyles'
 import AddSong from '../components/AddSong'
 import { Toast } from 'react-native-toast-message/lib/src/Toast'
@@ -16,36 +16,37 @@ import { getListController } from '../utils/ApiUtils'
 export default function YourSongs() {
     let [playlists, setPlaylists] = useState<CoflnetSongVoterModelsPlayList[]>([])
     let [showAddSongModal, setShowAddSongModal] = useState(false)
+    let [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         loadPlaylists()
     }, [])
 
     async function loadPlaylists() {
-        let listController = await getListController()
-        let playlists = await listController.listsGet()
-        console.log(JSON.stringify(playlists))
+        try {
+            setIsLoading(true)
+            let listController = await getListController()
+            let playlists = await listController.listsGet()
+            console.log(JSON.stringify(playlists))
 
-        if (playlists.length === 0) {
-            await listController.listsPost({
-                coflnetSongVoterModelsPlayList: {
-                    title: 'Default Playlist',
-                    songs: []
-                }
-            })
-            playlists = await listController.listsGet()
+            if (playlists.length === 0) {
+                await listController.listsPost({
+                    coflnetSongVoterModelsPlayList: {
+                        title: 'Default Playlist',
+                        songs: []
+                    }
+                })
+                playlists = await listController.listsGet()
+            }
+            setPlaylists(playlists)
+        } catch (e) {
+            showErrorToast(e)
+        } finally {
+            setIsLoading(false)
         }
-        setPlaylists(playlists)
     }
 
     async function onSongAdded(song: CoflnetSongVoterModelsSong) {
-        console.log(
-            'song: ' +
-                JSON.stringify({
-                    listId: playlists[0].id,
-                    body: song.id
-                })
-        )
         try {
             let listController = await getListController()
             await listController.listsListIdSongsPost({
@@ -61,6 +62,8 @@ export default function YourSongs() {
             text1: 'Song added',
             text2: song.title
         })
+        setPlaylists([])
+        loadPlaylists()
     }
 
     /*
@@ -75,6 +78,7 @@ export default function YourSongs() {
     return (
         <MainLayout>
             <HeaderText text="Your Songs" />
+            {isLoading ? <ActivityIndicator size="large" /> : null}
             <FAB
                 icon="plus"
                 label="Add song"
